e5185a528cd51d4cb600ce8dcab78d9f65acbb89
Misael Josias Marron lope <mmarronl@unsa.edu.pe>
2023-05-08 19:06:49 -0500

    Primera version , falta mucho

--- lab01/Ingresar_Datos.java
+++ lab01/Ingresar_Datos.java
@@ -1,6 +1,23 @@
+
+import java.util.*;
+
 public class Ingresar_Datos {
   public static void main(String[] args) {
+	Scanner scan = new Scanner(System.in);
+
     	System.out.println("Buenos Dias amig@ \nIngrese su nombre ");
-	
+	String nombre = scan.nextLine();
+
+	System.out.println("Ingrese su edad: ");
+	int edad = scan.nextInt();
+
+	System.out.println("Ingrese su peso: ");
+	double peso = scan.nextDouble();
+
+	System.out.println("Ingrese su altura : ");		
+	double altura = scan.nextDouble();
+
+	Persona p1 = new Persona(nombre,peso,altura,edad);
+	System.out.println(p1);
   }
 }
