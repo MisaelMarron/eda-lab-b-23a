9cfa597a2d5f5d077491c75607622e1a12df0f06
Misael Josias Marron lope <mmarronl@unsa.edu.pe>
2023-05-08 19:01:24 -0500

    avance en clase ver1

--- lab01/Persona.java
+++ lab01/Persona.java
@@ -1,6 +1,30 @@
 public class Persona {
-  private String nombre ="";
-  private double peso;
-  private int	
+   private String nombre ="";
+   private double peso;
+   private double IMC = 0;	 
+   private double altura
+   private int edad;
  
+   public Persona( String n , double p , double a , int edad ){
+	this.nombre = n ;
+	this.peso = p;
+	this.altura = a;
+	this.edad = edad
+ 
+   }	
+   
+   public Persona(){
+    super("Jhon Doe", 0.0 , 0.0 , 1 );
+   }
+   
+   public void setIMC(){
+	IMC = peso/altura*altura;
+
+   }
+
+   public String toString(){
+	return nombre + "\t" + edad + "\t" + peso + "\t" + altura + "\t" + IMC
+
+   }
+
 }
