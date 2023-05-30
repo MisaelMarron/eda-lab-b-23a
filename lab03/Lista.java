024e48665cbea91cd1e0153dee2db4d0d92ab6b1
Misael Marron <mmarronl@unsa.edu.pe>
2023-05-30 11:53:27 -0500

    agregamos la clase Nodo y cambiamos metodos

--- lab03/Lista.java
+++ lab03/Lista.java
@@ -1,9 +1,8 @@
 public interface Lista<E> {
-	void insertFirst(E x);
-	void insertLast(E x);
-	boolean search(E x);
-	void remove(E x);
-	int lenght();
+	
+	void add(E x);
+	boolean contains(E x);
+	void remove(E x);	
 	boolean isEmpty();
 	int size();
 }
---
+++ lab03/Nodo.java
@@ -0,0 +1,27 @@
+public class Nodo<E> {
+	private E data;
+	private Nodo<E> next;
+	
+	public Nodo(E data, Nodo<E> next) {
+		this.data = data;
+		this.next = next;
+	}
+	public Nodo(E data) {
+		this(data, null);
+	}
+	public E getData() {
+		return this.data;
+	}
+	public void setData(E data) {
+		this.data = data;
+	}
+	public Nodo<E> getNext() {
+		return this.next;
+	}
+	public void setNext(Nodo<E> next) {
+		this.next = next;
+	}
+	public String toString() {
+		return this.data.toString();
+	}
+}
