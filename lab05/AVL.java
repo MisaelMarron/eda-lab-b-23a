d52306143615d5d7d17e81c73fda2cf5f7ac95e7
Misael Marron <mmarronl@unsa.edu.pe>
2023-06-26 00:44:38 -0500

    Creamos clase AVL

---
+++ lab05/AVL.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+import org.graphstream.ui.view.Viewer;
+
+
+public class AVL<T extends Comparable<T>> {
+    private Nodo<T> root;
+    
+    public AVL() {
+        root = null;
+    }
+    
+
+    
+}
