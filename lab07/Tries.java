15adde1e658a46e650aa2ccb72386f77c5c6dc19
Misael Marron <mmarronl@unsa.edu.pe>
2023-07-23 22:52:55 -0500

    Agregamos la clase embebida TrieNode

---
+++ lab07/Tries.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class TrieNode {
+
+    private static final int ALPHABET_SIZE = 30;
+
+    private TrieNode[] children;
+    private boolean isEndOfWord;
+
+    public TrieNode() {
+        children = new TrieNode[ALPHABET_SIZE];
+        isEndOfWord = false;
+    }
+
+    public TrieNode[] getChildren() {
+        return children;
+    }
+
+    public TrieNode getChild(char ch) {
+        int index = ch -'a';
+        if (index <0 || index >= ALPHABET_SIZE) {
+            return null;
+        }
+        return children[index];
+    }
+
+    public void setChild(char ch, TrieNode node) {
+        int index = ch - 'a';
+        if (index < 0 || index >= ALPHABET_SIZE) {
+            return;
+        }
+        children[index] = node;
+    }
+
+    public boolean isEndOfWord() {
+        return isEndOfWord;
+    }
+
+    public void setEndOfWord(boolean endOfWord) {
+        isEndOfWord = endOfWord;
+    }
+}
